fd65e61d363a5aeedaabaeedd68e5f8352e16251
ryotanr <ryotanr@amazon.com>
2024-01-06 18:10:01 +0900

    all

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,8 +1,19 @@
 Chef::Log.info("********** Deploy! **********")
 
-execute "run application" do
-  command <<-EOH
-    pwd
-    ls
-  EOH
-end
+node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  current_dir = ::File.join(deploy[:deploy_to], 'current')
+
+  execute "run application" do
+    command <<-EOH
+      python main.py
+    EOH
+  end
